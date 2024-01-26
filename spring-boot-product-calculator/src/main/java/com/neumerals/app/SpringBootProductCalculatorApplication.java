65a39fd72c7dd73a455416651e9c36abd6173a8d
sachindu <srilalwms1995@gmail.com>
2024-01-26 14:23:30 +0530

    Initial commit

---
+++ spring-boot-product-calculator/src/main/java/com/neumerals/app/SpringBootProductCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.neumerals.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductCalculatorApplication.class, args);
+	}
+
+}
---
+++ spring-boot-product-calculator/src/test/java/com/neumerals/app/SpringBootProductCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neumerals.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
